6ed252aded5f051ab160849ec901ffb295298be5
Phelan Lewis <phelanlewis@gmail.com>
2017-11-23 23:23:34 -0800

    added the cheerio to scrape the div.info

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+var fs      = require('fs')
 var request = require('request');
 var cheerio = require('cheerio');
 
 request('http://www.torontoneighbourhoods.net/suburbs/durham/pickering?real-estate', function(error, response, html){
   if (!error && response.statusCode === 200){
     var $ = cheerio.load(html);
-    $('div.price').each(function(i, element){
+    $('div.info').each(function(i, element){
       var data = $(this);
       console.log(data.text())
     })
+
   }
 })
