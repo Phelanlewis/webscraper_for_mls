6d79e75f9829b1d1e811dde3d01a594e911a6e39
Phelan Lewis <phelanlewis@gmail.com>
2017-11-26 22:52:26 -0800

    prices from the website scrape and but into price.html

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ var cheerio = require('cheerio');
 
 function scrapePage() {
   request('http://www.torontoneighbourhoods.net/neighbourhoods/east-end/leslieville?real-estate', function(error, response, html){
-    // if (!error && response.statusCode === 200){
-    //   var $ = cheerio.load(html);
-    //   $('div.info').each(function(i, element){
-    //     var data = $(this);
-    //     console.log(data.text())
-    //   })
 
-    fs.writeFile(__dirname + "/example.html", html, function(error){
-      console.log('example.html is added to directory')
+    var $ = cheerio.load(html);
+
+    var $price = $('div.info').each(function(i, element){
+        var data = $(this);
+        console.log(data.text())
+      })
+
+    //example version of all the html beong added
+    // fs.writeFile(__dirname + "/example.html", html, function(error){
+    //   console.log('example.html is added to directory')
+    // })
+
+    //adding the $price var to an html file
+
+    fs.writeFile(__dirname + "/price.html", $price, function(error){
+      console.log('added the prices.html is added to directory')
     })
+
   })
 }
 
