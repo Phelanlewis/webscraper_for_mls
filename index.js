e73ede9d17c998ef022a21be823f796b392b38d6
Phelan Lewis <phelanlewis@gmail.com>
2017-11-26 22:07:00 -0800

    added the fs write function

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ var fs      = require('fs')
 var request = require('request');
 var cheerio = require('cheerio');
 
-request('http://www.torontoneighbourhoods.net/suburbs/durham/pickering?real-estate', function(error, response, html){
-  if (!error && response.statusCode === 200){
-    var $ = cheerio.load(html);
-    $('div.info').each(function(i, element){
-      var data = $(this);
-      console.log(data.text())
-    })
 
-  }
-})
+function scrapePage() {
+  request('http://www.torontoneighbourhoods.net/neighbourhoods/east-end/leslieville?real-estate', function(error, response, html){
+    // if (!error && response.statusCode === 200){
+    //   var $ = cheerio.load(html);
+    //   $('div.info').each(function(i, element){
+    //     var data = $(this);
+    //     console.log(data.text())
+    //   })
+
+    fs.writeFile(__dirname + "/example.html", html, function(error){
+      console.log('example.html is added to directory')
+    })
+  })
+}
+
+scrapePage();
